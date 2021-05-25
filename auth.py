90f25ac3f21bd86f93759c76e5324a70facf527f
karine <karinefernandaaraujo@gmail.com>
2021-05-25 16:45:06 -0300

    first commit

---
+++ auth.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from app import app
+from flask_basicauth import BasicAuth
+
+
+
+app.config['BASIC_AUTH_USERNAME'] = 'admin'
+app.config['BASIC_AUTH_PASSWORD'] = '1234'
+
+basic_auth = BasicAuth(app)
+
+app.config['BASIC_AUTH_FORCE'] = True
+
+basic_auth.init_app(app)
